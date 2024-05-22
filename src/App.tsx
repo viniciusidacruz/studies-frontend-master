bee8363f0de4df742ac855d03225a8a2780bf4ab
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2024-05-22 14:50:58 -0300

    feat: implementando tailwindcss para estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,5 @@
-export const App = () => <h1>Hello World</h1>;
+export const App = () => (
+  <main className="h-screen w-full flex items-center justify-center">
+    <h1 className="text-4xl text-slate-800">Hello World</h1>
+  </main>
+);
