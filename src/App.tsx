317d95ca1923016f9739891f961649e285ab899c
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2024-05-23 14:51:28 -0300

    feat:  Implementando shadcn ui e importações de componentes necessário

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardFooter,
+  CardContent,
+  CardDescription,
+} from "@view/components";
+
 export const App = () => (
   <main className="h-screen w-full flex items-center justify-center">
-    <h1 className="text-4xl text-slate-800">Hello World</h1>
+    <Card>
+      <CardHeader>
+        <CardTitle>This is my title</CardTitle>
+        <CardDescription>This is my description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>This is my content</p>
+      </CardContent>
+      <CardFooter>
+        <p>This is my title footer</p>
+      </CardFooter>
+    </Card>
   </main>
 );
---
+++ src/view/components/index.ts
@@ -0,0 +1,6 @@
+export * from "./external/card";
+export * from "./external/avatar";
+export * from "./external/badge";
+export * from "./external/button";
+export * from "./external/separator";
+export * from "./external/skeleton";
