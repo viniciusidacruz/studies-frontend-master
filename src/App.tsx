47c1c09b3a80a782460a4eb9a2a0f707ab3621df
Vinicius Italo da Cruz <viniciusidacruz@hotmail.com>
2024-05-24 11:31:24 -0300

    feat: Inserção do componente de postagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import {
-  Card,
-  CardTitle,
-  CardHeader,
-  CardFooter,
-  CardContent,
-  CardDescription,
-} from "@presentation/components";
+import { CardPost } from "@presentation/components";
 
 export const App = () => (
   <main className="h-screen w-full flex items-center justify-center">
-    <Card>
-      <CardHeader>
-        <CardTitle>This is my title</CardTitle>
-        <CardDescription>This is my description</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>This is my content</p>
-      </CardContent>
-      <CardFooter>
-        <p>This is my title footer</p>
-      </CardFooter>
-    </Card>
+    <CardPost
+      title="Primeiro titulo"
+      content="lorem ipsum dolor sit amet, consectetur adip occurence velit"
+      createdAt="2024-05-12"
+      pathname="/post/um-titulo-interessante"
+      className="w-[300px]"
+      author={{
+        avatar: "https://github.com/viniciusidacruz.png",
+        name: "Vinicius Italo",
+      }}
+      description="Subtitulo"
+    />
   </main>
 );
